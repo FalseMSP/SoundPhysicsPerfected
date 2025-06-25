dcb6f4f93d55cda35e253e5ee1c27f937993e436
FalseMSP <mark.philip21@gmail.com>
2025-06-25 08:44:44 -0400

    inital commit (template Minecraft Mod)

---
+++ src/main/kotlin/redsmods/SoundPhysicsPerfected.kt
@@ -0,0 +1,15 @@
+package redsmods
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object SoundPhysicsPerfected : ModInitializer {
+    private val logger = LoggerFactory.getLogger("soundphysicsperfected")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
