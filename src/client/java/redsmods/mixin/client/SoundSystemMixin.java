1bb0e814156ad0621f1c6f5c5e9fe1f91394f231
FalseMSP <mark.philip21@gmail.com>
2025-07-23 19:36:30 -0400

    this took longer than i care to admit.

--- src/client/java/redsmods/mixin/client/SoundSystemMixin.java
+++ src/client/java/redsmods/mixin/client/SoundSystemMixin.java
@@ -25,7 +25,6 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
 
-import static java.lang.Math.clamp;
 import static org.joml.Math.lerp;
 import static org.lwjgl.openal.EXTEfx.*;
 import static redsmods.RaycastingHelper.*;
@@ -382,5 +381,9 @@ public abstract class SoundSystemMixin {
             }
         }
         System.out.println("Sources currently in use: " + sourcesInUse);
+    }
+
+    private static float clamp(float a, float b, float c) {
+        return Math.min(Math.max(a,b),c);
     }
 }
