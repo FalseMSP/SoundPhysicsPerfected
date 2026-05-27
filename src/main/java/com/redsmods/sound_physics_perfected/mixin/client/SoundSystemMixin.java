1ff0dd2eecb70d748d399afa3e7fca90cfa7acd0
FalseMSP <mark.philip21@gmail.com>
2026-05-27 13:36:07 -0400

    made priority of this mod lower so it gets triggered end of chain

--- src/main/java/com/redsmods/sound_physics_perfected/mixin/client/SoundSystemMixin.java
+++ src/main/java/com/redsmods/sound_physics_perfected/mixin/client/SoundSystemMixin.java
@@ -46,7 +46,7 @@ import static com.redsmods.sound_physics_perfected.RaycastingHelper.*;
 import static org.joml.Math.lerp;
 import static org.lwjgl.openal.EXTEfx.*;
 
-@Mixin(SoundEngine.class)
+@Mixin(value = SoundEngine.class, priority = 9001) // its over 9000!!!!
 public abstract class SoundSystemMixin {
 
     @Shadow
@@ -81,7 +81,7 @@ public abstract class SoundSystemMixin {
 
         try {
             WeighedSoundEvents weightedSoundSet = sound.resolve(soundManager); // load pitches and whatnot into the sound data
-            if (!(sound instanceof RedPositionedSoundInstance || sound instanceof TickableSoundInstance || sound instanceof RedPermeatedSoundInstance) && sound.getAttenuation() != SoundInstance.Attenuation.NONE) { // !replayList.contains(redSoundData)
+            if (!(sound instanceof RedPositionedSoundInstance || sound instanceof RedTickableInstance) && sound.getAttenuation() != SoundInstance.Attenuation.NONE) { // !replayList.contains(redSoundData)
                 // Get sound coordinates
                 double soundX = sound.getX();
                 double soundY = sound.getY();
