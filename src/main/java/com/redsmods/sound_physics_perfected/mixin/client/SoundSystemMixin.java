39c5170da3e8c81a7db13b0a88cf84c24a08767d
FalseMSP <mark.philip21@gmail.com>
2026-06-11 21:04:28 -0400

    not quite working, at least no crash

--- src/main/java/com/redsmods/sound_physics_perfected/mixin/client/SoundSystemMixin.java
+++ src/main/java/com/redsmods/sound_physics_perfected/mixin/client/SoundSystemMixin.java
@@ -14,6 +14,7 @@ import com.redsmods.sound_physics_perfected.wrappers.RedPermeatedSoundInstance;
 import com.redsmods.sound_physics_perfected.wrappers.RedPositionedSoundInstance;
 import com.redsmods.sound_physics_perfected.wrappers.RedTickableInstance;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.sounds.AbstractTickableSoundInstance;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.client.resources.sounds.TickableSoundInstance;
 import net.minecraft.client.sounds.ChannelAccess;
